cd7ea1993694b4cb7cd5d127e8732f2f4f8600b7
iamagf <61362029+iam-agf@users.noreply.github.com>
2021-10-08 23:05:28 +0200

    cameraPi
    Script to take a series of pictures that contains the QR id of the container and a series of pictures of the content of it. 
    
    Needs qr_decomposer.py to run.

---
+++ camera/cameraPi.py
@@ -0,0 +1,32 @@
+from picamera import PiCamera
+from time import sleep
+from storing_information import savePath
+from qr_decomposer import qr_decomposer
+import datetime 
+
+
+def takePictures():
+    actualTime = datetime.datetime.now()
+    actualTimeString = actualTime.strftime("%d_%m_%Y_%H_%M_%S")[:-4]
+    qrCapture = savePath + f"container_QR_code_{actualTimeString}.jpeg"
+
+    camera = PiCamera()
+    camera.rotation = 180
+    camera.start_preview()
+    camera.capture(qrCapture)
+    containerId = qr_decomposer(qrCapture)
+    sleep(2)
+
+    imagesNames = []
+    for idx in range(1):
+        sleep(2)
+        imageName = savePath + f"image{idx}_{containerId}_{actualTimeString}.jpeg"
+        camera.capture(imageName)
+        imagesNames.append(imageName)
+
+    camera.stop_preview()
+    return imagesNames, containerId, actualTimeString
+
+
+if  __name__ == '__main__':
+    takePictures()
